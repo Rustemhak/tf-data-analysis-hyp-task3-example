39fbbd7e31faca7251a58f257f9ab3cf01c89e47
Rustem <61783564+Rustemhak@users.noreply.github.com>
2023-04-12 13:45:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
+from scipy.stats import permutation_test
 
 chat_id = 326525586 # Ваш chat ID, не меняйте название переменной
 
@@ -8,6 +9,8 @@ def solution(x: np.array, y: np.array) -> bool: # Одна или две выб�
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, p_value = ttest_ind(x, y, equal_var=False)
+    p_value = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis),
+                                            vectorized=True,
+                                            n_resamples=5000).pvalue
     alpha = 0.07
     return p_value < alpha # Ваш ответ, True или False
